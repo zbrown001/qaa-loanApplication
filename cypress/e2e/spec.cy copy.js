5950ea795add1bfe76ff88419ed6a53659d0e376
zbrown001 <zbrown@nanolos.com>
2022-09-15 12:06:31 -0600

    Commit

---
+++ cypress/e2e/spec.cy copy.js
@@ -0,0 +1,34 @@
+// npx cypress run --record --key b44d18cc-21e5-45ae-8acb-88b350ec5a27
+
+import { faker } from '@faker-js/faker'
+
+
+describe('Automation Test Suite - Fixtures', function () {
+
+  // Get Parameters
+  before(function () {
+    cy.fixture('example.json').then(function (data) {
+      this.data = data;
+    })
+  })
+
+  it('Cypress Test Case - Understanding Fixtures', function () {
+    
+    // Faker Variables
+    const emailDigits = faker.random.numeric(8);
+
+    // Go to application
+    cy.clearCookies()
+    cy.clearLocalStorage()
+    cy.reload()
+    cy.visit(this.data.application);
+
+    
+
+    cy.xpath('//a[@data-qaa="loginGetStarted"]').click()
+    cy.xpath('//a[normalize-space()="Create Account"]').click()
+    cy.xpath('//input[@placeholder="yours@example.com"]').type('test-'+emailDigits+'@nanolos.com')
+    cy.xpath('//input[@placeholder="your password"]').type('Password!11')
+    cy.xpath('//span[@class="auth0-label-submit"]').click()
+  })
+})

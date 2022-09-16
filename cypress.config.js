4725719eddb83e167f1223c270be3ae6e5d9a236
zbrown001 <zbrown@nanolos.com>
2022-09-16 15:24:31 -0600

    Add recording

--- cypress.config.js
+++ cypress.config.js
@@ -3,11 +3,9 @@ const { defineConfig } = require("cypress")
 module.exports = defineConfig({
   chromeWebSecurity: false,
   projectId: "m6w88w",
-  env: {
-    CYPRESS_RECORD_KEY: 'b44d18cc-21e5-45ae-8acb-88b350ec5a27'
-  },
   e2e: {
     setupNodeEvents(on, config) {
+
     },
   },
 })

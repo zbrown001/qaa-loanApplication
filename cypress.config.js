1436f1d2505e535704d4ae4fcf91a593e8a32a08
zbrown001 <zbrown@nanolos.com>
2022-09-15 19:57:38 -0600

    fix shi

--- cypress.config.js
+++ cypress.config.js
@@ -1,6 +1,9 @@
 module.exports = {
   chromeWebSecurity: false,
   projectId: "m6w88w",
+  env: {
+    CYPRESS_RECORD_KEY: 'b44d18cc-21e5-45ae-8acb-88b350ec5a27'
+  },
   e2e: {
     setupNodeEvents(on, config) {
     },
